6dcc3fdc4572bc2879b6bd6db8f7b26e432a1cfd
Akhara <43671530+Student2003@users.noreply.github.com>
2021-05-02 14:36:00 +0200

    {{modify}}

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const path = __dirname + '/phonekata/public/index.html';
+const path = __dirname + '/phonekata/public/';
 
 const app = express();
 
