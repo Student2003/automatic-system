f511f85f596745de6fbe6d65bfefbbe2c0c35334
Akhara <43671530+Student2003@users.noreply.github.com>
2021-05-02 13:31:24 +0200

    {{modify controller and routter}}

--- phoneapp/controllers/controller.js
+++ phoneapp/controllers/controller.js
@@ -1,3 +1,32 @@
 const db = require("../models");
-const Tutorial = db.tutorials;
+const Contact = db.contacts;
 const Op = db.Sequelize.Op;
+
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.first_name) {
+      res.status(400).send({
+        message: "Please "
+      });
+      return;
+    }
+  
+    // Create a Contact
+    const contact = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      number: req.body.number,
+    };
+  
+    // Save Contact in the database
+    Contact.create(contact)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the Contact."
+        });
+      });
+  };
--- phoneapp/models/model.js
+++ phoneapp/models/model.js
@@ -11,5 +11,5 @@ module.exports = (sequelize, Sequelize) => {
       }
     });
   
-    return Tutorial;
+    return Contact;
   };
