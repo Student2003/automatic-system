b8cfc254d9ffbcbc4d9f4ccb4a6f2f5f328c7dc0
Akhara <43671530+Student2003@users.noreply.github.com>
2021-05-02 13:58:43 +0200

    {{modify}}

--- phoneapp/routes/route.js
+++ phoneapp/routes/route.js
@@ -17,7 +17,7 @@ module.exports = app => {
     // Retrieve a single Tutorial with id
     router.get("/:id", tutorials.findOne);
   
-    // Update a Tutorial with id
+    // Update a Contacts with id
     router.put("/:id", contacts.update);
   
     // Delete a Tutorial with id
